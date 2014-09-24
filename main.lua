e87aafecab9f6c2ed8d7715f43eb6a4f27ae12a1
Filipe Silva <filipe.av.silva@gmail.com>
2014-09-25 00:55:40 +0200

    Load map into game
    Still no collision detection.
    Game window adapts to map

--- main.lua
+++ main.lua
@@ -1,23 +1,34 @@
 local character = require("character")
+local maps = require("maps")
 
 local MOVE_STEP = 10
 
-local pacman
+local dude
+local map
 
 function love.load() --[[***************************]]--
+    if arg[#arg] == "-debug" then require("mobdebug").start() end
     love.graphics.setBackgroundColor(0, 0, 0)
 
-    local image = love.graphics.newImage("img/Pac.png")
-    pacman = character.create(image, 100, 100, MOVE_STEP)
+    local charImage = love.graphics.newImage("img/Red.png")
+    dude = character.create(charImage, 100, 100, MOVE_STEP)
+
+    local wallImage = love.graphics.newImage("img/wall.png")
+    local powerImage = love.graphics.newImage("img/power.png")
+    map = maps.load("maps/map_wide.pacmap", wallImage, powerImage)
+
+    local dimensions = map:getDimensions()
+    love.window.setMode(dimensions.width, dimensions.height)
 end
 
 
 function love.update(timeDelta) --[[***************************]]--
-    pacman:move()
+    dude:move()
 end
 
 
 function love.draw() --[[***************************]]--
-    pacman:draw()
+    map:draw()
+    dude:draw()
 end
 
